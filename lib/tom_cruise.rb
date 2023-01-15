7a42ce8b8f7863df4a974ce5fddbaddee4450078
Darren Cauthon <darren@cauthon.com>
2023-01-15 09:57:01 -0600

    Isolating.

--- lib/tom_cruise.rb
+++ lib/tom_cruise.rb
@@ -2,9 +2,16 @@
 
 require_relative 'tom_cruise/version'
 
-class TomCruise
+class TomCruise # rubocop:disable Style/Documentation
   def self.method_added(name)
     super
+
+    return if ['initialize'].include?(name)
+    return if self.name == 'TomCruise'
+
     puts "Added #{name}"
+  end
+
+  def under
   end
 end
