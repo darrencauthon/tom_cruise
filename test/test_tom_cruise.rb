abd974fec114087ac94305c84495dc2ae8d7e29c
Darren Cauthon <darren@cauthon.com>
2023-01-15 09:47:39 -0600

    Remove bad test.

--- test/test_tom_cruise.rb
+++ test/test_tom_cruise.rb
@@ -6,8 +6,4 @@ class TestTomCruise < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TomCruise::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
